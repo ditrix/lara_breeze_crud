3536158008ec03ac495ff47ecc72216e1b851007
dmvoloshin <ditrix2006@gmail.com>
2024-07-10 11:45:02 +0300

    test show

--- tests/Feature/ApiCompanyTest.php
+++ tests/Feature/ApiCompanyTest.php
@@ -49,4 +49,37 @@ class ApiCompanyTest extends TestCase
         $this->assertDatabaseHas('companies', $data);
     }
 
+    public function test_get_company(): void
+    {
+        $data =  [
+            'name' => 'dmitry voloshin',
+            'email' => 'dmitru@mail.com',
+            'address' => 'dmitry addr',
+            'website' => 'http://www.dmitry.com'
+        ];
+
+        $response = $this->postJson('api/companies', $data);
+
+        $response = $this->getJson('api/companies', [1]);
+
+
+
+        $response->assertStatus(200)
+        ->assertJson([
+            'data' =>
+              ['0' => [
+                'id'    => 2,
+                'name' => 'dmitry voloshin',
+                'email' => 'dmitru@mail.com',
+                'address' => 'dmitry addr',
+                'website' => 'http://www.dmitry.com'
+
+              ]
+            ]
+        ]);
+
+        $this->assertDatabaseHas('companies', $data);
+    }
+
+
 }
